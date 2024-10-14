8c27e8914897ff1572008559ed1416006ddad418
David Santana <davidsantana06@hotmail.com>
2024-10-14 13:57:41 -0300

    feat(client): add presentation and termination

--- client/app.py
+++ client/app.py
@@ -1,5 +1,5 @@
 from base64 import b64encode
-from colorama import Fore, Style
+from colorama import Fore
 from io import BytesIO
 from PIL import ImageGrab
 from pynput.keyboard import Key, Listener
@@ -23,38 +23,55 @@ def print_screen() -> str:
         return base64.decode('utf-8')
 
 
-# feedback _
+# communication _
+
+def display_presentation() -> None:
+    print(
+        f'{Fore.WHITE} * Serving WebSocket Client\n'
+        f'{Fore.YELLOW}Press PRINTSCREEN to send screenshot or ESC to quit'
+    )
+
 
 def handle_emission(
     event: Literal['success', 'error'],
     message: str
 ) -> None:
     color = Fore.GREEN if event == 'success' else Fore.RED
-    print(f'{color}{message}{Style.RESET_ALL}')
+    print(f'{color}{message}')
 
-
-# execution _
 
 if __name__ == '__main__':
-    # step 1: initialize colorama
-    colorama.init()
+    # _ initialize colorama
+    colorama.init(autoreset=True)
 
-    # step 2: instantiate socket client
+    # _ display presentation (obviously)
+    display_presentation()
+
+    # _ instantiate socketio client
     socket_io = Client()
 
-    # step 3: define event handlers
+    # _ define event handlers
     for event in ('success', 'error'):
-        handler = lambda message: handle_emission(event, message)
+        handler = lambda message, event=event: handle_emission(
+            event, 
+            message
+        )
         socket_io.on(event, handler)
 
-    # step 4: connect to server
+    # _ connect to server
     socket_io.connect(SERVER_HOST)
 
-    # step 5 onwards: listen for print screen press
+    # _ define key events
     def on_press(key: Key) -> None:
+        # _ press print screen - send screenshot
         if key == Key.print_screen:
             base64 = print_screen()
             socket_io.emit('print_screen', base64)
-            
-    with Listener(on_press) as listener:
+        # _ press esc - disconnect and terminate the program
+        elif key == Key.esc:
+            socket_io.disconnect()
+            exit(0)
+
+    # _ listen for keyboard key press
+    with Listener(on_press=on_press) as listener:
         listener.join()
